1cbfccfc23bd7f637b2d2f23f1983e8e37328e2f
Camille Reynders <camille.reynders@gmail.com>
2015-03-16 15:38:15 +0100

    Code style fixes

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-"use strict";
+'use strict';
 
-var _ = require( "lodash" ),
-	di = require( "asyncdi" ),
-	async = require( "async" );
+var _ = require('lodash'),
+	di = require('asyncdi'),
+	async = require('async');
 
-function init( opts ){
+function init(opts) {
 	this.__hooks = {
-		pre       : {},
-		post      : {},
-		originals : {},
-		opts      : opts || {}
+		pre      : {},
+		post     : {},
+		originals: {},
+		opts     : opts || {}
 	};
 }
 
-function addHook( context,
-				  eventType,
-				  args ){
+function addHook(context, eventType, args) {
 	var eventName = args.shift(),
-		fn = _.flatten( args );
-	if( !context.__hooks[ eventType ][ eventName ] && context.__hooks.opts.strict ){
-		throw new Error( 'Hooks for ' + eventName + ' are not supported.' );
+		fn = _.flatten(args);
+	if (!context.__hooks[eventType][eventName] && context.__hooks.opts.strict) {
+		throw new Error('Hooks for ' + eventName + ' are not supported.');
 	}
-	var handlers = context.__hooks[ eventType ];
-	handlers[ eventName ] = handlers[ eventName ].concat( fn );
+	var handlers = context.__hooks[eventType];
+	handlers[eventName] = handlers[eventName].concat(fn);
 }
 
-function parseTypedEvent( typedEvent ){
-	var temp = typedEvent.split( ":" );
+function parseTypedEvent(typedEvent) {
+	var temp = typedEvent.split(':');
 	return {
-		type : temp[ 0 ],
-		name : temp[ 1 ]
+		type: temp[0],
+		name: temp[1]
 	};
 }
 var methods = {
@@ -40,9 +38,8 @@ var methods = {
 	 * @param {(...Function|Function[])} fn - function(s) to call
 	 * @api public
 	 */
-	pre : function( event,
-					fn ){
-		addHook( this, "pre", _.toArray( arguments ) );
+	pre: function() {
+		addHook(this, 'pre', _.toArray(arguments));
 		return this;
 	},
 
@@ -53,9 +50,8 @@ var methods = {
 	 * @param {(...Function|Function[])} fn - function(s) to call
 	 * @api public
 	 */
-	post : function( event,
-					 fn ){
-		addHook( this, "post", _.toArray( arguments ) );
+	post: function() {
+		addHook(this, 'post', _.toArray(arguments));
 		return this;
 	},
 
@@ -66,12 +62,11 @@ var methods = {
 	 * @param {(...Function|Function[])} fn - function(s) to call
 	 * @api public
 	 */
-	hookup : function( typedEvent,
-					   fn ){
-		var event = parseTypedEvent( typedEvent );
-		var args = _.toArray( arguments );
-		args[ 0 ] = event.name;
-		this[ event.type ].apply( this, args );
+	hookup: function(typedEvent) {
+		var event = parseTypedEvent(typedEvent),
+			args = _.toArray(arguments);
+		args[0] = event.name;
+		this[event.type].apply(this, args);
 		return this;
 	},
 
@@ -80,21 +75,21 @@ var methods = {
 	 * @param {(...string|string[])} types - namespaced event(s), e.g. `pre:save`
 	 * or simply passing `save` will register both `pre:save` and `post:save`
 	 */
-	hookable : function( types ){
-		var args = _.flatten( _.toArray( arguments ) );
-		_.each( args, function( typedEvent ){
-			var event = parseTypedEvent( typedEvent );
-			if( !this.__hooks[ event.type ] ){
-				if( event.name ){
-					throw new Error( "Only 'pre' and 'post' types are allowed, not '" + event.type + "'" );
+	hookable: function() {
+		var args = _.flatten(_.toArray(arguments));
+		_.each(args, function(typedEvent) {
+			var event = parseTypedEvent(typedEvent);
+			if (!this.__hooks[event.type]) {
+				if (event.name) {
+					throw new Error('Only "pre" and "post" types are allowed, not "' + event.type + '"');
 				} else {
-					this.__hooks.pre[ event.type ] = [];
-					this.__hooks.post[ event.type ] = [];
+					this.__hooks.pre[event.type] = [];
+					this.__hooks.post[event.type] = [];
 				}
 			} else {
-				this.__hooks[ event.type ][ event.name ] = [];
+				this.__hooks[event.type][event.name] = [];
 			}
-		}, this );
+		}, this);
 		return this;
 	},
 
@@ -105,16 +100,13 @@ var methods = {
 	 * @param {Function} iteratee - the function to apply to the hooks
 	 * @param {Function} [done] - will be called when all hooks have been called
 	 */
-	hooks : function( context,
-					  typedEvent,
-					  iteratee,
-					  done ){
-		var args = _.toArray( arguments );
-		if( _.isString( context ) ){
-			args.unshift( null );
+	hooks: function() {
+		var args = _.toArray(arguments);
+		if (_.isString(args[0])) {
+			args.unshift(null);
 		}
-		var event = parseTypedEvent( args[ 1 ] );
-		async.eachSeries( this.__hooks[ event.type ][ event.name ], args[ 2 ].bind( args[ 0 ] ), args[ 3 ] );
+		var event = parseTypedEvent(args[1]);
+		async.eachSeries(this.__hooks[event.type][event.name], args[2].bind(args[0]), args[3]);
 		return this;
 	},
 
@@ -125,28 +117,25 @@ var methods = {
 	 * @param {...*} [parameters] - any parameters you wish to pass to the hooks.
 	 * @param {Function} [callback] - will be called when all hooks have finished
 	 */
-	emit : function( context,
-					 typedEvent,
-					 parameters ){
-		var args = _.toArray( arguments ),
+	emit: function(context, typedEvent) {
+		var args = _.toArray(arguments),
 			done;
-		if( _.isString( context ) ){
+		if (_.isString(context)) {
 			typedEvent = context;
 			context = null;
 		} else {
 			args.shift(); //drop `context`
 		}
 		args.shift();//drop `typedEvent`
-		if( _.isFunction( args[ args.length - 1 ] ) ){
+		if (_.isFunction(args[args.length - 1])) {
 			done = args.pop(); //drop callback
 		}
-		args = _.flatten( args );// in case parameters were passed in as an array; this is 2-dim, we need 1-dim
-		this.hooks( typedEvent, function( hook,
-										  next ){
-			di( hook, context, { callbackParamName : "next"} ).provides( args ).call( function(){
+		args = _.flatten(args);// in case parameters were passed in as an array; this is 2-dim, we need 1-dim
+		this.hooks(typedEvent, function(hook, next) {
+			di(hook, context, {callbackParamName: 'next'}).provides(args).call(function() {
 				next();
-			} );
-		}, done );
+			});
+		}, done);
 		return this;
 	},
 
@@ -167,19 +156,18 @@ var methods = {
 	 * @param {String} [typedEvent] - namespaced event, e.g. `pre:save`
 	 * @param {(...Function|Function[])} [fn] - function(s) to be removed
 	 */
-	unhook : function( typedEvent,
-					   fn ){
-		var methods = _.toArray( arguments );
-		var event = parseTypedEvent( methods.shift() );
-		if( methods.length ){
-			_.remove( this.__hooks[ event.type ][ event.name ], function( hook ){
-				return methods.indexOf( hook ) > -1;
-			}, this );
+	unhook: function() {
+		var methods = _.toArray(arguments);
+		var event = parseTypedEvent(methods.shift());
+		if (methods.length) {
+			_.remove(this.__hooks[event.type][event.name], function(hook) {
+				return methods.indexOf(hook) > -1;
+			}, this);
 		} else {
-			if( event.name ){
-				delete this.__hooks[ event.type ][ event.name ];
+			if (event.name) {
+				delete this.__hooks[event.type][event.name];
 			} else {
-				this.__hooks[ event.type ] = {};
+				this.__hooks[event.type] = {};
 			}
 		}
 		return this;
@@ -188,32 +176,31 @@ var methods = {
 	/**
 	 * Wraps `method` with `pre` and `post` emission
 	 * @example
-	 * field.hooked("save");
-	 * field.pre("save", function(){
-	 *   console.log("pre:save called");
+	 * field.hooked('save');
+	 * field.pre('save', function(){
+	 *   console.log('pre:save called');
 	 * });
 	 * field.save();
 	 * @param {(...String|String[])} method - method(s) that need(s) to emit `pre` and `post` events
 	 */
-	hooked : function( method ){
-		var methods = _.flatten( _.toArray( arguments ) );
-		_.each( methods, function( method ){
-			this.hookable( method );
-			this.__hooks.originals[ method ] = this[ method ];
-			this[ method ] = function(){
-				this.emit( "pre:" + method );
-				this.__hooks.originals[ method ].apply( this, _.toArray( arguments ) );
-				this.emit( "post:" + method );
-			}.bind( this );
-		}, this );
+	hooked: function() {
+		var methods = _.flatten(_.toArray(arguments));
+		_.each(methods, function(method) {
+			this.hookable(method);
+			this.__hooks.originals[method] = this[method];
+			this[method] = function() {
+				this.emit('pre:' + method);
+				this.__hooks.originals[method].apply(this, _.toArray(arguments));
+				this.emit('post:' + method);
+			}.bind(this);
+		}, this);
 		return this;
 	}
 };
 
-function mixin( instance,
-				opts ){
-	init.call( instance, opts );
-	_.extend( instance, methods );
+function mixin(instance, opts) {
+	init.call(instance, opts);
+	_.extend(instance, methods);
 	return instance;
 }
 
@@ -223,11 +210,11 @@ function mixin( instance,
  * @param {Boolean} [opts.strict=true] - Will disallow subscribing to hooks bar the explicitly registered ones.
  * @returns {*}
  */
-function create( opts ){
-	return mixin( {}, opts );
+function create(opts) {
+	return mixin({}, opts);
 }
 
 module.exports = {
-	mixin  : mixin,
-	create : create
+	mixin : mixin,
+	create: create
 };
