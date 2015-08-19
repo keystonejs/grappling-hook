8259f4359eb056a001b9b071232121c556eb0691
Camille Reynders <camille.reynders@gmail.com>
2015-08-19 11:01:42 +0200

    100% test coverage

---
+++ tests/options-createThenable.spec.js
@@ -0,0 +1,25 @@
+'use strict';
+/* eslint-env node, mocha */
+
+var expect = require('must');
+var P = require('bluebird');
+
+var subject = require('../index');
+var $ = require('./fixtures');
+var NOOP = function() {
+
+};
+
+describe('options: `createThenable`', function() {
+	var instance;
+	describe('default', function() {
+		beforeEach(function() {
+			instance = subject.create({strict:false});
+		});
+		it('it should throw an error when trying to use thenable hooks', function() {
+			expect(function() {
+				instance.callThenableHook('pre:test');
+			}).to.throw(/createThenable/);
+		});
+	});
+});
